78fb95f4ad1f88a74711fc828d0701b2a26d9276
christiano0407 <placebo0704@gmail.com>
2024-04-28 17:28:54 -0600

    add new language

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+// en => / es => /es (Language)
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()], 
+  i18n: {
+    defaultLocale: 'en', 
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
---
+++ src/i18n/index.ts
@@ -0,0 +1,10 @@
+import english from '../i18n/en.json'
+import spanish from '../i18n/es.json'
+
+const LANGUAGE = {
+  ENGLISH: "en", 
+  SPANISH: "es"
+}
+
+
+export const getI18N = ({currentLocale}) => {}
