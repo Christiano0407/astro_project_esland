ddfe0ceefe73bcc0a4d9aaeb13152d28b71a61db
christiano0407 <placebo0704@gmail.com>
2024-04-28 17:37:29 -0600

    add new design with astro

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,14 @@
 import english from '../i18n/en.json'
 import spanish from '../i18n/es.json'
 
+
 const LANGUAGE = {
   ENGLISH: "en", 
   SPANISH: "es"
 }
 
 
-export const getI18N = ({currentLocale}) => {}
+export const getI18N = ({ currentlocale }: {currentLocale: 'en' | 'es'}) => {
+  if (currentlocale === LANGUAGE.ENGLISH) return english
+  if(currentlocale === LANGUAGE.SPANISH) return spanish
+}
